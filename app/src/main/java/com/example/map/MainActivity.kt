652b5bad2fe00d713aa238351e9a8cb733df1305
rozak <rozak.sn@gmailcom>
2024-05-14 22:14:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/map/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.map
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.map.databinding.ActivityMainBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private val binding by lazy(LazyThreadSafetyMode.NONE) {
+       ActivityMainBinding.inflate(layoutInflater)
+    }
+    private lateinit var mMaps:GoogleMap
+    private lateinit var database: DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val  mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this@MainActivity)
+        database = FirebaseDatabase.getInstance().reference
+
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMaps = googleMap
+        fetchLocationData()
+
+    }
+
+    private fun fetchLocationData(){
+        database.child("location").addValueEventListener(object :ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (locationSnapshot in snapshot.children) {
+                    val latitude = locationSnapshot.child("lat").getValue().toString().toDouble()
+                    val longitude = locationSnapshot.child("lon").getValue().toString().toDouble()
+
+                    if (latitude != null && longitude != null) {
+                        val location = LatLng(latitude, longitude)
+                        mMaps.addMarker(MarkerOptions().position(location).title("Marker"))
+                        mMaps.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15f))
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
+}
